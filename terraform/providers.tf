d5e3f300b5908f66fa0b8df34c5f28a59562c8f3
berordas <202214793@alu.comillas.edu>
2026-04-27 13:44:29 +0200

    Completar placeholders.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("<placeholder>")
+  credentials = file("../icai2025-494410-2e50f75ecf7e.json")
 
   project = var.gcp-project
   region  = var.gcp-region
